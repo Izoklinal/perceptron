90884882d8ca656d8dd92d4613b17c36e921858b
antybody <denissavicin@gmail.com>
2026-07-04 10:26:08 +0500

    fixed bugs

--- main.cpp
+++ main.cpp
@@ -39,7 +39,6 @@ int main() {
     cellSize = drawingPxs / data.header.pic_size_x;
 
     std::cout << "Training..." << std::endl;
-    uint8_t target = 3;
     Perceptron perceptron = Perceptron(0.001, target);
     perceptron.train(500, data);
     std::cout << "Training ended!" << std::endl;
@@ -198,54 +197,3 @@ void render(const Picture& pictureToRender) {
     EndMode2D();
     EndDrawing();
 }
-
-// void render(const Pictures& data, size_t idx, uint8_t target, uint8_t predict) {
-//     ClearBackground(BLACK);
-//     BeginDrawing();
-//     BeginMode2D(cam);
-//     {
-//         if (paintingMode == false) {
-//             DrawRectangleLines(100, 100, 280, 280, WHITE);
-//             std::string idx_text = "idx:" + std::to_string(idx + 1);
-//             DrawText(idx_text.c_str(), 10, 10, 30, WHITE);
-            
-//             Color lcol = WHITE;
-//             if (target == data.labels[idx]) {
-//                 lcol = GREEN;
-//             }
-//             std::string labels_text = "num:" + std::to_string(data.labels[idx]);
-//             DrawText(labels_text.c_str(), 10, 40, 30, lcol);
-//         }
-
-//         Color pcol = WHITE;
-//         if (target == data.labels[idx] && predict == 1) {
-//             pcol = GREEN;
-//         } else if (target == data.labels[idx] && predict == 0) {
-//             pcol = RED;
-//         }
-//         std::string predict_text = "predict:" + std::to_string(predict);
-//         DrawText(predict_text.c_str(), 10, 70, 30, pcol);
-        
-//         for (size_t y = 0; y < 28; y++)
-//         {
-//             for (size_t x = 0; x < 28; x++)
-//             {
-//                 if (paintingMode) {
-
-//                 }   
-//                 else {
-//                     Color c = Color{
-//                         .r = 255,
-//                         .g = 255,
-//                         .b = 255,
-//                         .a = data.pictures[idx].pixels[y][x]
-//                     };
-//                     DrawRectangle(100 + x*10, 100 + y*10, 10, 10, c);
-//                 }
-//             }
-//         }
-        
-//     }
-//     EndMode2D();
-//     EndDrawing();
-// }
